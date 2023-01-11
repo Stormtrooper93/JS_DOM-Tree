8fa85571a1ed829b24e52d337196f6d00e2f152a
Stormtrooper93 <vvetall@gmail.com>
2023-01-10 19:01:05 -0500

    #

--- First homework/script.js
+++ First homework/script.js
@@ -10,11 +10,11 @@
 let domNavigation = document.getElementById('headerTwo').textContent;
 console.log(domNavigation);
 
-let section = document.getElementsByTagName('section').item(0);
+let section = document.getElementsByTagName('section')[0];
 console.log(section);
 
 let fiveItem = document.querySelector('.item5').textContent;
 console.log(fiveItem);
 
-let hatredLevelBlock = document.getElementsByClassName('hatredLevelBlock').item(0);
+let hatredLevelBlock = document.getElementsByClassName('hatredLevelBlock')[0];
 console.log(hatredLevelBlock);
